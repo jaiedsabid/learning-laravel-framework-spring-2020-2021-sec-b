4ab5831dc40e2bd46f4ca6b6106ad43f669b04fd
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2021-02-16 23:43:39 +0600

    Created and added User model and database connection

--- laratest/app/Models/User.php
+++ laratest/app/Models/User.php
@@ -2,42 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasFactory, Notifiable;
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    /*
+    protected $table = null;
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    */
+    protected $primaryKey = "user_id";
+    public $timestamps = false;
+    //const CREATED_AT = "Create_Time";
+    //const UPDATED_AT = "Update_Time";
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+
+
+
 }
