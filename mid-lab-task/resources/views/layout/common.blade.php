202c8fd051d0c56a8fbd0ac1d6dd875d6d12c76a
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2021-03-01 14:03:43 +0600

    Added layout model

---
+++ mid-lab-task/resources/views/layout/common.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.main')
+    @section('nav-bar')
+        <ul>
+            <li><a href="{{ route('home.index') }}">Home</a></li>
+            <li><a href="{{ route('logout.index') }}">Logout</a></li>
+        </ul>
+    @endsection
+    @section('footer')
+        Copyright 2021
+    @endsection
